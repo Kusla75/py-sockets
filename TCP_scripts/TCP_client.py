cacb370aa5786fcf4d0fbca5b7550a47359b447c
Kusla75 <nikola.kuslakovic1616@gmail.com>
2019-09-30 19:29:27 +0200

    Added comments

--- TCP_scripts/TCP_client.py
+++ TCP_scripts/TCP_client.py
@@ -1,14 +1,18 @@
 import socket
 
+# create client socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_ip = '192.168.56.1'
 port = 420
 
+# connect client socket to server using servers ip and port
 client_socket.connect((server_ip, port))
 
+# recieve message from server
 server_message = client_socket.recv(1024)
 
 client_socket.close()
 
+# decode and print recieved message
 print(server_message.decode('utf-8'))
--- TCP_scripts/TCP_server.py
+++ TCP_scripts/TCP_server.py
@@ -1,19 +1,26 @@
 import socket
 
+# create server socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# get name of host machine
 host = socket.gethostname()
 port = 420
 
+# bind host name and port to socket object
 server_socket.bind((host, port))
 
+# wait for new connections
 server_socket.listen(5)
 
 while True:
+    # accept new established connection
+    # returns socket object and (soc ip address, soc port)
     client_socket, address = server_socket.accept()
 
     print('Server succesfully connected to ' + str(address))
     message = 'Welcome to server!'
 
+    # decode and send message to client socket
     client_socket.send(message.encode('utf-8'))
     client_socket.close()
